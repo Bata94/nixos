c4124679b1ec950dc302d280a5f3ce8585aa7465
Bastian Sievers <b.sievers@sits.ruhr>
2024-12-18 18:36:03 +0100

    added docker feature
    s
    
    s

---
+++ hosts/features/services/docker/default.nix
@@ -0,0 +1,20 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  # hardware.nvidia-container-toolkit.enable = true;
+  virtualisation.docker = {
+    enable = true;
+    enableOnBoot = true;
+    storageDriver = "overlay2";
+    autoPrune.enable = true;
+    logDriver = "json-file";
+  };
+  users.users.bata.extraGroups = ["docker"];
+  virtualisation.oci-containers.backend = "docker";
+
+  environment.systemPackages = [
+    inputs.compose2nix.package.${pkgs.system}.default
+  ];
+}
