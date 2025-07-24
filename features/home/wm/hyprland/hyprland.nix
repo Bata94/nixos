a208ee4a04da4bd7fec41400a38303263f4556b5
bata <bastian.sievers@gmail.com>
2025-07-24 18:00:02 +0200

    Hyprland swapped ESC and CAPS

--- features/home/wm/hyprland/hyprland.nix
+++ features/home/wm/hyprland/hyprland.nix
@@ -176,7 +176,7 @@ in {
           kb_layout = "de";
           kb_variant = "";
           kb_model = "";
-          kb_options = "";
+          kb_options = "caps:swapescape";
           kb_rules = "";
           follow_mouse = 1;
 
