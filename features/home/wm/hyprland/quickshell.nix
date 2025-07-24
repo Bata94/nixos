8d635b4da4f2ac510978f6600ec8854f55fce929
bata <bastian.sievers@gmail.com>
2025-07-24 09:27:05 +0200

    minimal working quickshell via bata94/quickshell repo

--- features/home/wm/hyprland/quickshell.nix
+++ features/home/wm/hyprland/quickshell.nix
@@ -5,14 +5,50 @@ in {
   config = mkIf cfg.enable {
     # home.file.".config/quickshell/shell.qml".text = ''
     # '';
-    home.packages = with pkgs.kdePackages; [
-      qtsvg
-      qtimageformats
-      qtmultimedia
-      qt5compat
+    home.packages = with pkgs; [
+      ## Deps via quickshell (caelestia-shell)
+      ddcutil
+      # brightnessctl
+      app2unit
+      cava
+      networkmanager
+      lm_sensors
+      # grim
+      swappy
+      # wl-clipboard
+      libqalculate
+      inotify-tools
+      bluez
+      coreutils
+      findutils
+      file
+      ## Caelestia-cli
+      # swappy
+      libnotify
+      # slurp
+      # wl-clipboard
+      # cliphist
+      # app2unit
+      dart-sass
+      # grim
+      fuzzel
+      wl-screenrec
+      dconf
+      # killall
+      ## Div
+    ] ++ [
+      pkgs.kdePackages.qtsvg
+      pkgs.kdePackages.qtimageformats
+      pkgs.kdePackages.qtmultimedia
+      pkgs.kdePackages.qt5compat
 
-      kiconthemes
-      breeze-icons
+      pkgs.kdePackages.kiconthemes
+      pkgs.kdePackages.breeze-icons
+    ] ++ [
+      python3.pkgs.hatch-vcs
+      python3.pkgs.hatchling
+      python3.pkgs.materialyoucolor
+      python3.pkgs.pillow
     ] ++ [ inputs.quickshell.packages.${pkgs.system}.default ];
   };
 }
