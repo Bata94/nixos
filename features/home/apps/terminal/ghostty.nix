56022f6e1ce0448416fd6b5499e8338068a4240e
bata <bastian.sievers@gmail.com>
2025-08-04 16:30:56 +0200

    macos ghostty changes

--- features/home/apps/terminal/ghostty.nix
+++ features/home/apps/terminal/ghostty.nix
@@ -4,11 +4,13 @@
   lib,
   ...
 }:
-with lib; let
+with lib;
+let
   cfg = config.features.home.apps.terminal.ghostty;
-in {
+in
+{
   options.features.home.apps.terminal.ghostty.enable = mkEnableOption "Enable Ghostty";
-  options.features.home.apps.terminal.ghostty.installPackage= mkOption {
+  options.features.home.apps.terminal.ghostty.installPackage = mkOption {
     type = types.bool;
     default = true;
   };
@@ -50,35 +52,37 @@ in {
       };
     };
 
+    home.file."Library/Application\ Support/com.mitchellh.ghostty/config".text =
+      mkIf pkgs.stdenv.isDarwin ''
+        theme = "rose-pine"
+        window-theme = "ghostty"
 
-    home.file."Library/Application\ Support/com.mitchellh.ghostty/config".text = mkIf pkgs.stdenv.isDarwin ''
-      theme = "rose-pine"
-      window-theme = "ghostty"
+        mouse-scroll-multiplier = 0.5
 
-      mouse-scroll-multiplier = 0.5
+        background = "000000"
+        background-opacity = 0.8
+        background-blur-radius = 20
 
-      background = "000000"
-      background-opacity = 0.8
-      background-blur-radius = 20
+        gtk-titlebar = false
+        gtk-tabs-location = "hidden"
+        gtk-single-instance = true
 
-      gtk-titlebar = false
-      gtk-tabs-location = "hidden"
-      gtk-single-instance = true
+        macos-option-as-alt = true
 
-      font-size = 9.0
-      font-family = "FiraMono Nerd Font"
+        font-size = 9.0
+        font-family = "FiraMono Nerd Font"
 
-      keybind = "alt+1=unbind"
-      keybind = "alt+2=unbind"
-      keybind = "alt+3=unbind"
-      keybind = "alt+4=unbind"
-      keybind = "alt+5=unbind"
-      keybind = "alt+6=unbind"
-      keybind = "alt+7=unbind"
-      keybind = "alt+8=unbind"
-      keybind = "alt+9=unbind"
-      keybind = "alt+0=unbind"
-      keybind = "alt+t=unbind"
-    '';
+        keybind = "alt+1=unbind"
+        keybind = "alt+2=unbind"
+        keybind = "alt+3=unbind"
+        keybind = "alt+4=unbind"
+        keybind = "alt+5=unbind"
+        keybind = "alt+6=unbind"
+        keybind = "alt+7=unbind"
+        keybind = "alt+8=unbind"
+        keybind = "alt+9=unbind"
+        keybind = "alt+0=unbind"
+        keybind = "alt+t=unbind"
+      '';
   };
 }
